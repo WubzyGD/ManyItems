54be031e65c0e29eaa13f7b94e55538041b2f833
WubzyGD <wubzygd@gmail.com>
2020-08-16 18:22:15 -0600

    Initial

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+export class Weapon {
+    name: String;
+    baseDamage: Number;
+    constructor (name: String, baseDamage: Number) {
+        this.name = name;
+        this.baseDamage = baseDamage;
+    };
+};
